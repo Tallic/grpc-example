316e15eeb5fad8b6ab91d4cee00ee2c60855461f
Hannes Thaller <hannes.thaller.at@gmail.com>
2017-07-17 13:20:16 +0200

    Made skeletons for the presentation

--- kotlin-professor/src/main/kotlin/at/jku/isse/psma/tutorial/grpc/Classroom.kt
+++ kotlin-professor/src/main/kotlin/at/jku/isse/psma/tutorial/grpc/Classroom.kt
@@ -11,26 +11,4 @@ class Classroom(private val port: Int,
         private val logger = LoggerFactory.getLogger(Classroom::class.java)
     }
 
-    private val server: Server = ServerBuilder.forPort(port)
-            .addService(service)
-            .build()
-
-    fun start() {
-        server.start()
-        logger.info("Server started, listening on $port")
-
-        Runtime.getRuntime().addShutdownHook(Thread(Runnable {
-            logger.error("shutting down gRpc server since JVM is shutting down")
-            stop()
-            logger.error("server shut down")
-        }))
-    }
-
-    fun stop() {
-        server.shutdown()
-    }
-
-    fun blockUnitShutdown() {
-        server.awaitTermination()
-    }
 }
--- kotlin-professor/src/main/kotlin/at/jku/isse/psma/tutorial/grpc/ProfessorService.kt
+++ kotlin-professor/src/main/kotlin/at/jku/isse/psma/tutorial/grpc/ProfessorService.kt
@@ -20,104 +20,11 @@ class ProfessorService(val name: String) : ProfessorServiceGrpc.ProfessorService
 
     private fun findAnswers(question: String): List<String> {
 
-        val answer = knowledge.entries
+        val answers = knowledge.entries
                 .filter { (key, _) -> question.toLowerCase().contains(key) }
                 .map { it.value }
 
-        return if (answer.isNotEmpty()) answer else listOf("I cannot help you with that.")
+        return if (answers.isNotEmpty()) answers else listOf("I cannot help you with that.")
     }
 
-    override fun askSimpleQuestion(request: Question, responseObserver: StreamObserver<Answer>) {
-        logger.info("Receiving new question from ${request.name} (${request.id})")
-
-        val answerText = findAnswers(request.text).first()
-
-        val answer = Answer.newBuilder()
-                .setId(request.id)
-                .setText(answerText)
-                .build()
-
-        responseObserver.onNext(answer)
-
-        responseObserver.onCompleted()
-
-        logger.info("Answered question ${request.id}")
-    }
-
-    override fun askMultipleSimpleQuestions(responseObserver: StreamObserver<Answer>): StreamObserver<Question> {
-        logger.info("Receiving multiple questions ... i will provide only one answer.")
-
-        return object : StreamObserver<Question> {
-            private var id = ""
-            private var answers = mutableListOf<String>()
-
-            override fun onError(t: Throwable) {
-                val status = Status.fromThrowable(t)
-                logger.error("Something disturbing happened in the classroom. ($status)", t)
-            }
-
-            override fun onNext(question: Question) {
-                if (id.isEmpty()) id = question.id
-
-                answers.add(findAnswers(question.text).first())
-            }
-
-            override fun onCompleted() {
-                val answer = Answer.newBuilder()
-                        .setId(id)
-                        .setText(answers.joinToString(" "))
-                        .build()
-
-                responseObserver.onNext(answer)
-
-                responseObserver.onCompleted()
-
-                logger.info("Answered question $id")
-            }
-        }
-    }
-
-    override fun askComplexQuestion(request: Question, responseObserver: StreamObserver<Answer>) {
-        logger.info("Receiving new question from ${request.name} (${request.id})")
-
-        findAnswers(request.text)
-                .forEach {
-                    val answer = Answer.newBuilder()
-                            .setId(request.id)
-                            .setText(it)
-                            .build()
-                    responseObserver.onNext(answer)
-                }
-
-        responseObserver.onCompleted()
-
-        logger.info("Answered question ${request.id}")
-    }
-
-    override fun askMultipleComplexQuestions(responseObserver: StreamObserver<Answer>): StreamObserver<Question> {
-        logger.info("Receiving multiple questions ... i will provide multiple answers.")
-
-        return object : StreamObserver<Question> {
-            override fun onError(t: Throwable) {
-                val status = Status.fromThrowable(t)
-                logger.error("Something disturbing happened in the classroom. ($status)", t)
-            }
-
-            override fun onNext(question: Question) {
-
-                val answer = Answer.newBuilder()
-                        .setId(question.id)
-                        .setText(findAnswers(question.text).joinToString(" "))
-                        .build()
-
-                responseObserver.onNext(answer)
-            }
-
-            override fun onCompleted() {
-                responseObserver.onCompleted()
-
-                logger.info("Answered all questions")
-            }
-        }
-    }
 }
